02f1e50877bd462e2196484b98a8dab2a3373a9a
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-03-27 09:48:28 +0800

    feature(mod): tidy up the module dependencies

---
+++ playground/main.go
@@ -0,0 +1,13 @@
+// Copyright © 2019 sammyne <xiangminli@alumni.sjtu.edu.cn>
+//
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file
+//
+
+package main
+
+import "github.com/sammyne/2018-mastering-ethereum/playground/cmd"
+
+func main() {
+	cmd.Execute()
+}
