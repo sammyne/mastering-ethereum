89dad1d400fbfaaf5ac73e16e4bba376b5415320
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-04-05 13:09:08 +0800

    style(chapter12/frontend): separate out the Jumbotron component

---
+++ chapter12-decentralized-applications/auction-dapp/frontend/components/Jumbotron.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card color="transparent" flat>
+    <v-card-text>
+      <v-layout align-center row justify-space-around>
+        <v-flex md5 sm12>
+          <h1 class="display-3">Auction Dapp</h1>
+          <h2 class="font-weight-light">Decentralized auction platform powered by Ethereum</h2>
+        </v-flex>
+        <v-flex md4 sm12>
+          <v-img max-width="320" src="~/assets/images/auction.svg"></v-img>
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
--- chapter12-decentralized-applications/auction-dapp/frontend/pages/index.vue
+++ chapter12-decentralized-applications/auction-dapp/frontend/pages/index.vue
@@ -1,25 +1,16 @@
 <template>
   <v-layout row>
     <v-flex sm12>
-      <v-card dark flat>
-        <v-card-text>
-          <v-layout row>
-            <v-flex sm5>
-              <h1>Auction Dapp</h1>
-              <h2>Decentralized auction platform powered by Ethereum</h2>
-            </v-flex>
-            <v-flex sm6>
-              <v-img src="~/assets/images/auction.svg"></v-img>
-            </v-flex>
-          </v-layout>
-        </v-card-text>
-      </v-card>
+      <jumbotron/>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import Jumbotron from "~/components/Jumbotron";
+
 export default {
-  layout: "dapp"
+  layout: "dapp",
+  components: { Jumbotron }
 };
 </script>
