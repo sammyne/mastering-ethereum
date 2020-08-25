581c547cad655c70e356afaa4ddb7ef250236293
sammyne <lixiangmin01@baidu.com>
2020-08-25 09:49:00 +0800

    feature(chapter09): upgrade example code to solidity@v0.7.0 for section 'Default Visibilities'

--- chapter09-smart-contract-security/examples/default-visibility/HashForEther.sol
+++ chapter09-smart-contract-security/examples/default-visibility/HashForEther.sol
@@ -1,14 +1,14 @@
-pragma solidity ^0.5.6;
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.7.0;
 
 contract HashForEther {
-
-    function withdrawWinnings() {
+    function withdrawWinnings() external {
         // Winner if the last 8 hex characters of the address are 0
         require(uint32(msg.sender) == 0);
         _sendWinnings();
-     }
+    }
 
-     function _sendWinnings() {
-         msg.sender.transfer(this.balance);
-     }
+    function _sendWinnings() {
+        msg.sender.transfer(address(this).balance);
+    }
 }
--- chapter09-smart-contract-security/examples/default-visibility/WalletLibrary.sol
+++ chapter09-smart-contract-security/examples/default-visibility/WalletLibrary.sol
@@ -1,34 +1,37 @@
-pragma solidity ^0.5.6;
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.7.0;
 
 contract WalletLibrary is WalletEvents {
+    //...
 
-  //...
+    // METHODS
 
-  // METHODS
+    //...
 
-  //...
-
-  // constructor is given number of sigs required to do protected
-  // "onlymanyowners" transactionsas well as the selection of addresses
-  // capable of confirming them
-  function initMultiowned(address[] _owners, uint _required) {
-    m_numOwners = _owners.length + 1;
-    m_owners[1] = uint(msg.sender);
-    m_ownerIndex[uint(msg.sender)] = 1;
-    for (uint i = 0; i < _owners.length; ++i)
-    {
-      m_owners[2 + i] = uint(_owners[i]);
-      m_ownerIndex[uint(_owners[i])] = 2 + i;
+    // constructor is given number of sigs required to do protected
+    // "onlymanyowners" transactionsas well as the selection of addresses
+    // capable of confirming them
+    function initMultiowned(address[] _owners, uint256 _required) {
+        m_numOwners = _owners.length + 1;
+        m_owners[1] = uint256(msg.sender);
+        m_ownerIndex[uint256(msg.sender)] = 1;
+        for (uint256 i = 0; i < _owners.length; ++i) {
+            m_owners[2 + i] = uint256(_owners[i]);
+            m_ownerIndex[uint256(_owners[i])] = 2 + i;
+        }
+        m_required = _required;
     }
-    m_required = _required;
-  }
 
-  //...
+    //...
 
-  // constructor - just pass on the owner array to multiowned and
-  // the limit to daylimit
-  function initWallet(address[] _owners, uint _required, uint _daylimit) {
-    initDaylimit(_daylimit);
-    initMultiowned(_owners, _required);
-  }
+    // constructor - just pass on the owner array to multiowned and
+    // the limit to daylimit
+    function initWallet(
+        address[] _owners,
+        uint256 _required,
+        uint256 _daylimit
+    ) {
+        initDaylimit(_daylimit);
+        initMultiowned(_owners, _required);
+    }
 }
